11c02ea6dcdd6568783b4192abea83bb576fcf4f
ACloud <zzc1529119384@163.com>
2026-06-15 16:03:13 +0800

    fix: admin-service 排除 RemoteStpInterfaceAutoConfig

--- ZXYZdatabaseBack/zxyz-admin-service/src/main/java/uno/acloud/admin/ZxyzAdminApplication.java
+++ ZXYZdatabaseBack/zxyz-admin-service/src/main/java/uno/acloud/admin/ZxyzAdminApplication.java
@@ -2,14 +2,17 @@ package uno.acloud.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import uno.acloud.common.config.RemoteStpInterfaceAutoConfig;
 
 @ServletComponentScan
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAutoConfiguration(exclude = RemoteStpInterfaceAutoConfig.class)
 @ComponentScan(basePackages = {"uno.acloud.admin", "uno.acloud.common"})
 @ConfigurationPropertiesScan
 public class ZxyzAdminApplication {
