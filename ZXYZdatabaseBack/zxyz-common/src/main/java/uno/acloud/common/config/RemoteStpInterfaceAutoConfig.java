2b150ccdf1b452bf3e901cf63b1e6377866b6715
ACloud <zzc1529119384@163.com>
2026-06-15 17:26:05 +0800

    fix: 补回 ConditionalOnProperty import

--- ZXYZdatabaseBack/zxyz-common/src/main/java/uno/acloud/common/config/RemoteStpInterfaceAutoConfig.java
+++ ZXYZdatabaseBack/zxyz-common/src/main/java/uno/acloud/common/config/RemoteStpInterfaceAutoConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
